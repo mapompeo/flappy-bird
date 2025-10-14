84082d727fdbcdb70f380c76f9f64502899df1f6
Matheus Pompeo <matheuspompeo13@gmail.com>
2025-10-14 20:33:39 -0300

    initializing the project

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BirdController : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Pular com espaço ou clique
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+
+        // Movimento lateral
+        Move();
+    }
+
+    void Jump()
+    {
+        // Zera a velocidade vertical antes de pular
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+
+        // Aplica força para cima
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    void Move()
+    {
+        // Pega input horizontal (A/D ou Setas)
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // Move o personagem mantendo a velocidade vertical
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+    }
+}
